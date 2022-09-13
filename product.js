dd86bd3e7d2a235e8a1364a311998506daab4b2e
Tom Billington <tombill143@gmail.com>
2022-09-13 14:19:24 +0200

    Dynaimc Product with database working

---
+++ product.js
@@ -0,0 +1,19 @@
+const url = "https://kea-alt-del.dk/t7/api/products/2801";
+//fetch the data
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
+//populate the page
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".breadcrumbs .brand").textContent = product.brandname;
+  document.querySelector(".breadcrumbs .productname").textContent =
+    product.productdisplayname;
+
+  document.querySelector(
+    "img.productimage"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+
+  document.querySelector("img.productimage").alt = product.productdisplayname;
+}
